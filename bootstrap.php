35e62c439f28c44b5157aa023554db925579d6ed
Sebastian Wróblewski <kontakt@swroblewski.pl>
2019-07-28 18:54:51 +0200

    Telegram notifications after the build.

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,10 @@
 <?php
 
-use TightenCo\Jigsaw\Jigsaw;
+use Illuminate\Container\Container;
+use TightenCo\Jigsaw\Events\EventBus;
 
-/** @var $container \Illuminate\Container\Container */
-/** @var $events \TightenCo\Jigsaw\Events\EventBus */
+/** @var $container Container */
+/** @var $events EventBus */
 
 /**
  * You can run custom code at different stages of the build process by
@@ -17,3 +18,4 @@ use TightenCo\Jigsaw\Jigsaw;
  */
 
 $events->afterBuild(App\Listeners\GenerateSitemap::class);
+$events->afterBuild(App\Listeners\NotifyTelegram::class);
