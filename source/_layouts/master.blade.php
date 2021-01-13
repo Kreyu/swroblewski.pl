5415ffd5d2d8da87ceeac788b62b987e23a6daca
Sebastian Wróblewski <kontakt@swroblewski.pl>
2021-01-13 22:26:58 +0100

    Remove kofi

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -73,10 +73,6 @@
                             </span>
                         </a>
                     </li>
-                    <li class="mx-3 hidden sm:block hover:text-orange-500 transition-faster">
-                        <script type='text/javascript' src='https://ko-fi.com/widgets/widget_2.js'></script>
-                        <script type='text/javascript'>kofiwidget2.init('Support Me on Ko-fi', '#2e2e2e', 'J3J42HTWT');kofiwidget2.draw();</script>
-                    </li>
                 </ul>
             </div>
         </nav>
